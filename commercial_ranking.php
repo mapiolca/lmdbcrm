f785a7dce4e5f49e5c773f195cefb5b23cda9faa
Pierre Ardoin <32256817+mapiolca@users.noreply.github.com>
2025-12-14 23:32:48 +0100

    Affiner les filtres du classement commerciaux

--- commercial_ranking.php
+++ commercial_ranking.php
@@ -1,28 +1,28 @@
 <?php
-/* Copyright (C) 2001-2005	Rodolphe Quiedeville	<rodolphe@quiedeville.org>
- * Copyright (C) 2004-2015	Laurent Destailleur	<eldy@users.sourceforge.net>
- * Copyright (C) 2005-2012	Regis Houssin	<regis.houssin@inodbox.com>
- * Copyright (C) 2025		Pierre Ardoin						<developpeur@lesmetiersdubatiment.fr>
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program. If not, see <https://www.gnu.org/licenses/>.
- */
+/* Copyright (C) 2001-2005Rodolphe Quiedeville<rodolphe@quiedeville.org>
+* Copyright (C) 2004-2015Laurent Destailleur<eldy@users.sourceforge.net>
+* Copyright (C) 2005-2012Regis Houssin<regis.houssin@inodbox.com>
+* Copyright (C) 2025Pierre Ardoin<developpeur@lesmetiersdubatiment.fr>
+*
+* This program is free software; you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation; either version 3 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program. If not, see <https://www.gnu.org/licenses/>.
+*/
 
 /**
- * \file       lmdbcrm/commercial_ranking.php
- * \ingroup    lmdbcrm
- * \brief      Ranking list for sales representatives based on proposals
- */
+* \file       lmdbcrm/commercial_ranking.php
+* \ingroup    lmdbcrm
+* \brief      Ranking list for sales representatives based on proposals
+*/
 
 // Load Dolibarr environment
 $res = 0;
@@ -60,12 +60,13 @@ if (!$res) {
 }
 
 /**
- * The main.inc.php has been included so the following variables are now defined:
- * @var Conf $conf
- * @var DoliDB $db
- * @var Translate $langs
- * @var User $user
- */
+* The main.inc.php has been included so the following variables are now defined:
+* @var Conf $conf
+* @var DoliDB $db
+* @var Translate $langs
+* @var User $user
+*/
+
 dol_include_once('/core/lib/date.lib.php');
 dol_include_once('/user/class/user.class.php');
 
@@ -84,21 +85,26 @@ if (empty($user->rights->propal->lire)) {
 }
 
 // Manage sorting and search parameters
-	$sortfield = GETPOST('sortfield', 'aZ09');
-	$sortorder = GETPOST('sortorder', 'aZ09');
+$sortfield = GETPOST('sortfield', 'aZ09');
+$sortorder = GETPOST('sortorder', 'aZ09');
 $validSortFields = array('total_count', 'signed_count', 'total_amount', 'signed_amount', 'conversion_rate', 'userid');
 
 $search_date_start_input = trim(GETPOST('search_date_start', 'alphanohtml'));
 $search_date_end_input = trim(GETPOST('search_date_end', 'alphanohtml'));
 
+$search_date_start = 0;
+if (!empty($search_date_start_input)) {
 	$search_date_start = dol_stringtotime($search_date_start_input);
-	$search_date_end = dol_stringtotime($search_date_end_input);
-
-if (empty($search_date_start_input)) {
-	$search_date_start = 0;
+	if ($search_date_start <= 0) {
+		$search_date_start = 0;
+	}
 }
-if (empty($search_date_end_input)) {
-	$search_date_end = 0;
+$search_date_end = 0;
+if (!empty($search_date_end_input)) {
+	$search_date_end = dol_stringtotime($search_date_end_input);
+	if ($search_date_end <= 0) {
+		$search_date_end = 0;
+	}
 }
 
 $search_user = trim(GETPOST('search_user', 'alphanohtml'));
@@ -123,13 +129,13 @@ if (!empty($search_date_start_input)) {
 if (!empty($search_date_end_input)) {
 	$param .= '&search_date_end='.urlencode($search_date_end_input);
 }
-if (!empty($search_user)) {
+if ($search_user !== '') {
 	$param .= '&search_user='.urlencode($search_user);
 }
-if (dol_strlen((string) $search_total_count)) {
+if ($search_total_count !== '') {
 	$param .= '&search_total_count='.urlencode($search_total_count);
 }
-if (dol_strlen((string) $search_signed_count)) {
+if ($search_signed_count !== '') {
 	$param .= '&search_signed_count='.urlencode($search_signed_count);
 }
 if ($search_total_amount !== '') {
@@ -147,39 +153,39 @@ $userstatic = new User($db);
 
 $title = $langs->trans('LmdbCrmSalesRepRanking');
 
-	llxHeader('', $title, '', '', 0, 0, '', '', '', 'mod-lmdbcrm page-commercial-ranking');
+llxHeader('', $title, '', '', 0, 0, '', '', '', 'mod-lmdbcrm page-commercial-ranking');
 
 // Title bar following Dolibarr list layout
-	print load_fiche_titre($title, '', 'chart');
-	print '<br>';
+print load_fiche_titre($title, '', 'chart');
+print '<br>';
 
 // Build SQL request for ranking
-	$sql = "SELECT u.rowid as userid, u.lastname, u.firstname, u.login, u.photo, u.email";
-	$sql .= ", COUNT(p.rowid) as total_count";
-	$sql .= ", SUM(CASE WHEN p.fk_statut IN (2, 4) THEN 1 ELSE 0 END) as signed_count";
-	$sql .= ", SUM(p.total_ht) as total_amount";
-	$sql .= ", SUM(CASE WHEN p.fk_statut IN (2, 4) THEN p.total_ht ELSE 0 END) as signed_amount";
-	$sql .= ", CASE WHEN COUNT(p.rowid) > 0 THEN (SUM(CASE WHEN p.fk_statut IN (2, 4) THEN 1 ELSE 0 END) / COUNT(p.rowid)) * 100 ELSE 0 END as conversion_rate";
-	$sql .= " FROM ".$db->prefix()."user as u";
-	$sql .= " JOIN ".$db->prefix()."propal as p ON p.fk_user_author = u.rowid";
-	$sql .= " WHERE p.fk_statut IN (1, 2, 3, 4)";
-	$sql .= " AND p.entity IN (".getEntity('propal').")";
-if (!empty($search_user)) {
+$sql = "SELECT u.rowid as userid, u.lastname, u.firstname, u.login, u.photo, u.email";
+$sql .= ", COUNT(p.rowid) as total_count";
+$sql .= ", SUM(CASE WHEN p.fk_statut IN (2, 4) THEN 1 ELSE 0 END) as signed_count";
+$sql .= ", SUM(p.total_ht) as total_amount";
+$sql .= ", SUM(CASE WHEN p.fk_statut IN (2, 4) THEN p.total_ht ELSE 0 END) as signed_amount";
+$sql .= ", CASE WHEN COUNT(p.rowid) > 0 THEN (SUM(CASE WHEN p.fk_statut IN (2, 4) THEN 1 ELSE 0 END) / COUNT(p.rowid)) * 100 ELSE 0 END as conversion_rate";
+$sql .= " FROM ".$db->prefix()."user as u";
+$sql .= " JOIN ".$db->prefix()."propal as p ON p.fk_user_author = u.rowid";
+$sql .= " WHERE p.fk_statut IN (1, 2, 3, 4)";
+$sql .= " AND p.entity IN (".getEntity('propal').")";
+if ($search_user !== '') {
 	$sql .= natural_search(array('u.lastname', 'u.firstname', 'u.login'), $search_user);
 }
-if (!empty($search_date_start)) {
+if ($search_date_start > 0) {
 	$sql .= " AND p.datep >= '".$db->idate($search_date_start)."'";
 }
-if (!empty($search_date_end)) {
+if ($search_date_end > 0) {
 	$sql .= " AND p.datep <= '".$db->idate($search_date_end)."'";
 }
-	$sql .= " GROUP BY u.rowid, u.lastname, u.firstname, u.login, u.photo, u.email";
+$sql .= " GROUP BY u.rowid, u.lastname, u.firstname, u.login, u.photo, u.email";
 
 $having = array();
-if (dol_strlen((string) $search_total_count)) {
+if ($search_total_count !== '') {
 	$having[] = " COUNT(p.rowid) = ".((int) $search_total_count);
 }
-if (dol_strlen((string) $search_signed_count)) {
+if ($search_signed_count !== '') {
 	$having[] = " SUM(CASE WHEN p.fk_statut IN (2, 4) THEN 1 ELSE 0 END) = ".((int) $search_signed_count);
 }
 if ($search_total_amount !== '') {
@@ -193,68 +199,67 @@ if ($search_conversion_rate !== '') {
 }
 
 if (!empty($having)) {
-	$sql .= " HAVING".implode(' AND', $having);
+	$sql .= " HAVING ".implode(' AND ', $having);
 }
 $sql .= $db->order($db->escape($sortfield), $db->escape($sortorder));
 
-	$resql = $db->query($sql);
+$resql = $db->query($sql);
 if (!$resql) {
 	dol_print_error($db);
 	exit();
 }
 
-	$num = $db->num_rows($resql);
+$num = $db->num_rows($resql);
 
 // Render list inspired by core proposal list layout
-	print '<form method="GET" action="'.$_SERVER['PHP_SELF'].'" name="search_form">';
-	print '<input type="hidden" name="sortfield" value="'.$sortfield.'">';
-	print '<input type="hidden" name="sortorder" value="'.$sortorder.'">';
+print '<form method="GET" action="'.$_SERVER['PHP_SELF'].'" name="search_form">';
+print '<input type="hidden" name="sortfield" value="'.$sortfield.'">';
+print '<input type="hidden" name="sortorder" value="'.$sortorder.'">';
 
-	print '<div class="div-table-responsive">';
-	print '<table class="tagtable liste">';
+print '<div class="div-table-responsive">';
+print '<table class="tagtable liste">';
 
-	print '<tr class="liste_titre_filter">';
-	print '<td class="liste_titre" colspan="6">';
-	print '<div class="nowraponall">';
-	print '<span class="opacitymedium">'.$langs->trans('PeriodRange').' : </span>';
-	print '<input class="flat datepicker" type="text" name="search_date_start" value="'.($search_date_start ? dol_print_date($search_date_start, 'day') : '').'" autocomplete="off">';
-	print ' - ';
-	print '<input class="flat datepicker" type="text" name="search_date_end" value="'.($search_date_end ? dol_print_date($search_date_end, 'day') : '').'" autocomplete="off">';
-	print '</div>';
-	print '</td>';
-	print '</tr>';
+print '<tr class="liste_titre_filter">';
+print '<td class="liste_titre" colspan="6">';
+print '<div class="nowraponall">';
+print '<span class="opacitymedium">'.$langs->trans('PeriodRange').' : </span>';
+print '<input class="flat datepicker" type="text" name="search_date_start" value="'.(!empty($search_date_start_input) ? dol_escape_htmltag($search_date_start_input) : '').'" autocomplete="off">';
+print ' - ';
+print '<input class="flat datepicker" type="text" name="search_date_end" value="'.(!empty($search_date_end_input) ? dol_escape_htmltag($search_date_end_input) : '').'" autocomplete="off">';
+print '</div>';
+print '</td>';
+print '</tr>';
 
-	print '<tr class="liste_titre_filter">';
-	print '<td class="liste_titre">';
-	print '<input class="flat" type="text" name="search_user" value="'.dol_escape_htmltag($search_user).'">';
-	print '</td>';
-	print '<td class="liste_titre">';
-	print '<input class="flat" type="text" name="search_total_count" value="'.(($search_total_count || $search_total_count === 0) ? dol_escape_htmltag($search_total_count) : '').'">';
-	print '</td>';
-	print '<td class="liste_titre">';
-	print '<input class="flat" type="text" name="search_signed_count" value="'.(($search_signed_count || $search_signed_count === 0) ? dol_escape_htmltag($search_signed_count) : '').'">';
-	print '</td>';
-	print '<td class="liste_titre">';
-	print '<input class="flat" type="text" name="search_total_amount" value="'.($search_total_amount !== '' ? dol_escape_htmltag($search_total_amount) : '').'">';
-	print '</td>';
-	print '<td class="liste_titre">';
-	print '<input class="flat" type="text" name="search_signed_amount" value="'.($search_signed_amount !== '' ? dol_escape_htmltag($search_signed_amount) : '').'">';
-	print '</td>';
-	print '<td class="liste_titre center">';
-	print '<input class="flat" type="text" name="search_conversion_rate" value="'.($search_conversion_rate !== '' ? dol_escape_htmltag($search_conversion_rate) : '').'">';
-	print $form->showFilterButtons('right');
-	print '</td>';
-	print '</tr>';
+print '<tr class="liste_titre_filter">';
+print '<td class="liste_titre">';
+print '<input class="flat" type="text" name="search_user" value="'.dol_escape_htmltag($search_user).'">';
+print '</td>';
+print '<td class="liste_titre">';
+print '<input class="flat" type="text" name="search_total_count" value="'.($search_total_count !== '' ? dol_escape_htmltag($search_total_count) : '').'">';
+print '</td>';
+print '<td class="liste_titre">';
+print '<input class="flat" type="text" name="search_signed_count" value="'.($search_signed_count !== '' ? dol_escape_htmltag($search_signed_count) : '').'">';
+print '</td>';
+print '<td class="liste_titre">';
+print '<input class="flat" type="text" name="search_total_amount" value="'.($search_total_amount !== '' ? dol_escape_htmltag($search_total_amount) : '').'">';
+print '</td>';
+print '<td class="liste_titre">';
+print '<input class="flat" type="text" name="search_signed_amount" value="'.($search_signed_amount !== '' ? dol_escape_htmltag($search_signed_amount) : '').'">';
+print '</td>';
+print '<td class="liste_titre center">';
+print '<input class="flat" type="text" name="search_conversion_rate" value="'.($search_conversion_rate !== '' ? dol_escape_htmltag($search_conversion_rate) : '').'">';
+print $form->showFilterButtons('right');
+print '</td>';
+print '</tr>';
 
-	print '<tr class="liste_titre">';
-	print_liste_field_titre($langs->trans('LmdbCrmSalesRep'), $_SERVER['PHP_SELF'], 'userid', '', $param, '', $sortfield, $sortorder);
-	print_liste_field_titre($langs->trans('LmdbCrmProposalsCount'), $_SERVER['PHP_SELF'], 'total_count', '', $param, '', $sortfield, $sortorder, 'center ');
-	print_liste_field_titre($langs->trans('LmdbCrmSignedProposalsCount'), $_SERVER['PHP_SELF'], 'signed_count', '', $param, '', $sortfield, $sortorder, 'center ');
-	print_liste_field_titre($langs->trans('LmdbCrmQuotedAmount'), $_SERVER['PHP_SELF'], 'total_amount', '', $param, '', $sortfield, $sortorder, 'right ');
-	print_liste_field_titre($langs->trans('LmdbCrmSignedAmount'), $_SERVER['PHP_SELF'], 'signed_amount', '', $param, '', $sortfield, $sortorder, 'right ');
-	print_liste_field_titre($langs->trans('LmdbCrmConversionRate'), $_SERVER['PHP_SELF'], 'conversion_rate', '', $param, '', $sortfield, $sortorder, 'center ');
-	print '</tr>';
-
+print '<tr class="liste_titre">';
+print_liste_field_titre($langs->trans('LmdbCrmSalesRep'), $_SERVER['PHP_SELF'], 'userid', '', $param, '', $sortfield, $sortorder);
+print_liste_field_titre($langs->trans('LmdbCrmProposalsCount'), $_SERVER['PHP_SELF'], 'total_count', '', $param, '', $sortfield, $sortorder, 'center ');
+print_liste_field_titre($langs->trans('LmdbCrmSignedProposalsCount'), $_SERVER['PHP_SELF'], 'signed_count', '', $param, '', $sortfield, $sortorder, 'center ');
+print_liste_field_titre($langs->trans('LmdbCrmQuotedAmount'), $_SERVER['PHP_SELF'], 'total_amount', '', $param, '', $sortfield, $sortorder, 'right ');
+print_liste_field_titre($langs->trans('LmdbCrmSignedAmount'), $_SERVER['PHP_SELF'], 'signed_amount', '', $param, '', $sortfield, $sortorder, 'right ');
+print_liste_field_titre($langs->trans('LmdbCrmConversionRate'), $_SERVER['PHP_SELF'], 'conversion_rate', '', $param, '', $sortfield, $sortorder, 'center ');
+print '</tr>';
 
 if ($num > 0) {
 	while ($obj = $db->fetch_object($resql)) {
@@ -278,9 +283,9 @@ if ($num > 0) {
 	print '<tr class="oddeven"><td colspan="6" class="opacitymedium center">'.$langs->trans('LmdbCrmNoRankingData').'</td></tr>';
 }
 
-	print '</table>';
-	print '</div>';
-	print '</form>';
+print '</table>';
+print '</div>';
+print '</form>';
 
 $db->free($resql);
 
