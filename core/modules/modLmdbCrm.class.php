a6da34f639a6fee8226c76bbb03e7b562d7dd9c0
Pierre Ardoin <32256817+mapiolca@users.noreply.github.com>
2025-12-16 22:18:23 +0100

    Update modLmdbCrm.class.php

--- core/modules/modLmdbCrm.class.php
+++ core/modules/modLmdbCrm.class.php
@@ -76,7 +76,7 @@ class modLmdbCrm extends DolibarrModules
 		$this->editor_squarred_logo = '';					// Must be image filename into the module/img directory followed with @modulename. Example: 'myimage.png@lmdbcrm'
 
 		// Possible values for version are: 'development', 'experimental', 'dolibarr', 'dolibarr_deprecated', 'experimental_deprecated' or a version string like 'x.y.z'
-		$this->version = '1.0';
+		$this->version = '1.1';
 		// Url to the file with your last numberversion of this module
 		//$this->url_last_version = 'http://www.example.com/versionmodule.txt';
 
@@ -239,6 +239,11 @@ class modLmdbCrm extends DolibarrModules
 			2 => array(
 				'file' => 'lmdbcrm_podium_signedturnover.php@lmdbcrm',
 				'note' => 'LmdbCrmSignedTurnoverPodium',
+				'enabledbydefaulton' => 'Home',
+			),
+			3 => array(
+				'file' => 'lmdbcrm_graph_marginrates.php@lmdbcrm',
+				'note' => 'LmdbcrmGraphMarginRatesDescription',
 				'enabledbydefaulton' => 'Home',
 			),
 		);
