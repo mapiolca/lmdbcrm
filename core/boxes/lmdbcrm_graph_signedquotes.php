fa0f1735b7d10e3700a4105494247c91b0e48153
Pierre Ardoin <32256817+mapiolca@users.noreply.github.com>
2026-01-08 13:44:53 +0100

    Update lmdbcrm_graph_signedquotes.php

--- core/boxes/lmdbcrm_graph_signedquotes.php
+++ core/boxes/lmdbcrm_graph_signedquotes.php
@@ -235,7 +235,7 @@ class lmdbcrm_graph_signedquotes extends ModeleBoxes
 			$graph->SetDataColor(array('#2e78c2', '#a3a3a3', '#2da44e', '#d8a200'));
 			$graph->SetType(array('lines'));
 			$graph->setHeight('320');
-			$graph->setWidth('740');
+			$graph->setWidth('720');
 			$graph->setShowLegend(1);
 			$graph->setMinValue(0);
 
